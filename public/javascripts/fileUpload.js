7aa5dd3ce4be186bdabc9cc719cbf5e110d41326
mohamed20medhat <egailmohamed2018@gmail.com>
2022-03-31 14:49:13 +0200

    finished the project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,34 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-);
+const rootStyle = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-    //the same as the aspect ratio of the image that we set when we made it pull it from the database in the books route -> books/index.ejs
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
-});
+if (
+    rootStyle.getPropertyValue("--book-cover-width-large") != null &&
+    rootStyle.getPropertyValue("--book-cover-width-large") !== '' ) {
+    ready();
+} else {
+    document.getElementById("main-css").addEventListener("load", ready);
+}
 
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(
+        rootStyle.getPropertyValue("--book-cover-width-large")
+    );
+    const coverAspectRatio = parseFloat(
+        rootStyle.getPropertyValue("--book-cover-aspect-ratio")
+    );
+    const coverHeight = parseFloat(coverWidth / coverAspectRatio);
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    );
+
+    FilePond.setOptions({
+        //the same as the aspect ratio of the image that we set when we made it pull it from the database in the books route -> books/index.ejs
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight,
+    });
+
+    FilePond.parse(document.body);
+}
